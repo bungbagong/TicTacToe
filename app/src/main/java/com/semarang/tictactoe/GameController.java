8923791b296f3d7a88b0e56552c9a3d4a9e2616b
bungbagong <bungbagong@gmail.com>
2015-08-17 21:25:39 +0800

    adding scanner for diagonal pattern

--- app/src/main/java/com/semarang/tictactoe/GameController.java
+++ app/src/main/java/com/semarang/tictactoe/GameController.java
@@ -73,39 +73,116 @@ public class GameController {
     /*
     Scan array in 3 directions
      */
-    private void scanArrayXO(){
+    private void scanArrayXO() {
         boolean[] winCell;
         int column = (int) Math.sqrt(mArrayXO.length);
         String extractString = "";
-        int winPosition[];
+        int winPosition[] = null;
+
+        //TODO diagonal logic algorithm optimization
+        //TODO change below logic to switch
 
         //scanning the rows
-        for (int i=0; i<numColumn; i++){  //number of rows = number of column
-            int start = i*numColumn;      //start at each horizontal row number
-            winPosition = scanWinRow(start,start+(numColumn-1));  //column-1 = length of the row
-            if (winPosition!= null) {
-                Log.d("semarang winRow = ", Arrays.toString(winPosition));
-                winArray = winPosition;
-                isWin = true;
-                break;
-            }
-        }
-
+        if (scanningTheRows()) return;
         //scanning the columns
-        for (int i=0; i<numColumn; i++){  //number of rows = number of column
+        if (scanningTheColumns()) return;
+        // diagonal from left to right
+        if (scanningDiagLeft_1st()) return;
+        if (scanningDiagLeft_2nd()) return;
+        // diagonal from right to left
+        if (scanningDiagRight_1st()) return;
+        if (scanningDiagRight_2nd()) return;
 
-            winPosition = scanWinColumn(i);
-            if (winPosition!= null) {
+
+    }
+
+    private boolean scanningDiagRight_2nd() {
+        int[] winPosition;
+        for (int i = numColumn - 1; i < (mArrayXO.length - (4 * numColumn)); i += 10) {
+            winPosition = scanWinDiagRight2(i);
+            if (winPosition != null) {
                 Log.d("semarang winCol = ", Arrays.toString(winPosition));
                 winArray = winPosition;
                 isWin = true;
-                break;
+
+                return true;
             }
         }
+        return false;
+    }
 
+    private boolean scanningDiagRight_1st() {
+        int[] winPosition;
+        for (int i = 4; i < (numColumn); i++) {
 
+                winPosition = scanWinDiagRight(i);
+                if (winPosition != null) {
+                    Log.d("semarang winCol = ", Arrays.toString(winPosition));
+                    winArray = winPosition;
+                    isWin = true;
+                    return true;
+                }
+            }
+        return false;
+    }
 
+    private boolean scanningDiagLeft_2nd() {
+        int[] winPosition;
+        for (int i = 0; i < (mArrayXO.length - (4 * numColumn)); i += 10) {
 
+                winPosition = scanWinDiagLeft2(i);
+                if (winPosition != null) {
+                    Log.d("semarang winCol = ", Arrays.toString(winPosition));
+                    winArray = winPosition;
+                    isWin = true;
+                    return true;
+                }
+            }
+        return false;
+    }
+
+    private boolean scanningDiagLeft_1st() {
+        int[] winPosition;
+        for (int i = 0; i < (numColumn - 4); i++) {
+
+            winPosition = scanWinDiagLeft(i);
+            if (winPosition != null) {
+                Log.d("semarang winCol = ", Arrays.toString(winPosition));
+                winArray = winPosition;
+                isWin = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean scanningTheColumns() {
+        int[] winPosition;
+        for (int i = 0; i < numColumn; i++) {  //number of rows = number of column
+            winPosition = scanWinColumn(i);
+            if (winPosition != null) {
+                Log.d("semarang winCol = ", Arrays.toString(winPosition));
+                winArray = winPosition;
+                isWin = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean scanningTheRows() {
+        int[] winPosition;
+        for (int i = 0; i < numColumn; i++) {  //number of rows = number of column
+            int start = i * numColumn;      //start at each horizontal row number
+            winPosition = scanWinRow(start, start + (numColumn - 1));  //column-1 = length of the row
+            if (winPosition != null) {
+                Log.d("semarang winRow = ", Arrays.toString(winPosition));
+                winArray = winPosition;
+                isWin = true;
+                return true;
+            }
+        }
+        return false;
     }
 
     /*
@@ -119,7 +196,7 @@ public class GameController {
         int startWinRow;
         int finishWinRow;
         int arrayWin[] = new int[5];
-        int arrayPos[] = new int[10];
+        int arrayPos[] = new int[numColumn];
 
         //setup pattern for reference
         String pattern = "XXXXX";
@@ -161,7 +238,7 @@ public class GameController {
         int startWinRow;
         int finishWinRow;
         int arrayWin[] = new int[5];
-        int arrayPos[] = new int[10];  //for translating pattern position to mArrayXO position
+        int arrayPos[] = new int[numColumn];  //for translating pattern position to mArrayXO position
 
         //setup pattern for reference
         String pattern = "XXXXX";
@@ -195,10 +272,176 @@ public class GameController {
 
     }
 
+    //scan for column in diagonal direction
+    private int[] scanWinDiagLeft(int start){
+
+        String extractString = "";
+        int startWinRow;
+        int finishWinRow;
+        int arrayWin[] = new int[5];
+        int arrayPos[] = new int[numColumn];  //for translating pattern position to mArrayXO position
+
+        //setup pattern for reference
+        String pattern = "XXXXX";
+        if (getXO().equals("O")){
+            pattern = "OOOOO";
+
+        }
+        //Log.d("semarang O or X=", pattern);
+
+        //add up the String in each cell in a column
+        // i = array no , j = used to populate arrayPos
+        for(int i = start, j = 0; i<=(mArrayXO.length-((10*start)+1)); i+=11, j++){
+            extractString += mArrayXO[i];
+            arrayPos[j]=i;
+        }
+        //Log.d("semarang stringcol=", extractString.toString());
+
+        //indexOf will return int position if substring pattern is found
+        int startPattern = extractString.indexOf(pattern);
+        if (startPattern==-1){ //if pattern not found will return -1
+
+            return null;
+
+
+        } else {
+
+            //translate start and finish position to array position
+            for (int i = 0; i < 5; i++){
+                arrayWin[i] = arrayPos[startPattern+i];
+            }
+
+            return arrayWin;
+        }
+
+    }
+
+    //scan for column in diagonal direction
+    private int[] scanWinDiagLeft2(int start){
+
+        String extractString = "";
+        int startWinRow;
+        int finishWinRow;
+        int arrayWin[] = new int[5];
+        int arrayPos[] = new int[numColumn];  //for translating pattern position to mArrayXO position
+
+        //setup pattern for reference
+        String pattern = "XXXXX";
+        if (getXO().equals("O")){
+            pattern = "OOOOO";
+
+        }
+        //Log.d("semarang O or X=", pattern);
+
+        //add up the String in each cell in a column
+        // i = cell no , j = used to populate arrayPos
+        for(int i = start, j = 0; i<=(mArrayXO.length-((start/10)+1)); i+=11, j++){  //TODO optimize this code
+            extractString += mArrayXO[i];
+            arrayPos[j]=i;
+        }
+        int startPattern2 = extractString.indexOf(pattern);
+        if (startPattern2 ==-1) { //if pattern not found will return -1
+            return null;
+
+        } else {
+            //translate start and finish position to array position
+            for (int i = 0; i < 5; i++){
+                arrayWin[i] = arrayPos[startPattern2+i];
+            }
+
+            return arrayWin;
+        }
+
+    }
 
 
 
 
+    //scan for column in diagonal direction
+    private int[] scanWinDiagRight(int start){
 
+        String extractString = "";
+        int startWinRow;
+        int finishWinRow;
+        int arrayWin[] = new int[5];
+        int arrayPos[] = new int[numColumn];  //for translating pattern position to mArrayXO position
+
+        //setup pattern for reference
+        String pattern = "XXXXX";
+        if (getXO().equals("O")){
+            pattern = "OOOOO";
+
+        }
+        //Log.d("semarang O or X=", pattern);
+
+        //add up the String in each cell in a column
+        // i = array no , j = used to populate arrayPos
+        for(int i = start, j = 0; i<=((10*start)); i+=9, j++){  //TODO optimize this code
+            extractString += mArrayXO[i];
+            arrayPos[j]=i;
+        }
+        //Log.d("semarang stringcol=", extractString.toString());
+
+        //indexOf will return int position if substring pattern is found
+        int startPattern = extractString.indexOf(pattern);
+        if (startPattern==-1){ //if pattern not found will return -1
+
+            return null;
+
+
+        } else {
+
+            //translate start and finish position to array position
+            for (int i = 0; i < 5; i++){
+                arrayWin[i] = arrayPos[startPattern+i];
+            }
+
+            return arrayWin;
+        }
+
+    }
+
+    //scan for column in diagonal direction
+    private int[] scanWinDiagRight2(int start){
+
+        String extractString = "";
+        int startWinRow;
+        int finishWinRow;
+        int arrayWin[] = new int[5];
+        int arrayPos[] = new int[numColumn];  //for translating pattern position to mArrayXO position
+
+        //setup pattern for reference
+        String pattern = "XXXXX";
+        if (getXO().equals("O")){
+            pattern = "OOOOO";
+
+        }
+        //Log.d("semarang O or X=", pattern);
+
+
+
+            //add up the String in each cell in a column
+            // i = array no , j = used to populate arrayPos
+            for(int i = start, j = 0; i<=(mArrayXO.length-4); i+=9, j++){ //TODO optimize this code
+                extractString += mArrayXO[i];
+                arrayPos[j]=i;
+            }
+            int startPattern2 = extractString.indexOf(pattern);
+            if (startPattern2 ==-1) { //if pattern not found will return -1
+                return null;
+
+            } else {
+                //translate start and finish position to array position
+                for (int i = 0; i < 5; i++){
+                    arrayWin[i] = arrayPos[startPattern2+i];
+                }
+
+                return arrayWin;
+            }
+
+
+
+
+    }
 
 }
